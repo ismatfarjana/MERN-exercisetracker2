61cadf22df7791f55be19d2009df7c351329aa4e
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-09-25 15:19:40 +1000

    create components

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CreateExercise extends Component {
+    render() {
+        return (
+            <div>
+                <p>in the create exercise page</p>
+            </div>
+        )
+    }
+}
