0d4f5ac4be94e6650b26209e0d1b4c9f4cde7169
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-09-25 14:36:51 +1000

    routes added

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -2,11 +2,12 @@ const router = require("express").Router();
 let Exercise = require("../models/exercise.model");
 
 router.route("/").get((req, res) => {
-    Exercise.find()
-        .then(exercises => res.json(exercises))
-        .catch(err => res.status(400).json("Error:" + err));
+    Exercise.find() //mongoose call back function, it will find all exercise
+        .then(exercises => res.json(exercises)) //read the json file
+        .catch(err => res.status(400).json("Error:" + err)); //if anything wrong , throw error
 });
 
+//route to add new exercise
 router.route("/add").post((req, res) => {
     const username = req.body.username;
     const description = req.body.description;
@@ -23,36 +24,38 @@ router.route("/add").post((req, res) => {
 
     newExercise
         .save()
-        .then(() => res.json("exercise added"))
+        .then(() => res.json("New exercise added to the log!"))
         .catch(err => res.status(400).json("error:" + err));
 });
 
-// router.route("/:id").get((req, res) => {
-//     Exercise.findById(req.params.id)
-//         .then(exercise => res.json(exercise))
-//         .catch(err => res.status(400).json("Error:" + err));
-// });
-//
-// router.route("/:id").delete((req, res) => {
-//     Exercise.findByIdAndDelete(req.params.id)
-//         .then(() => res.json("Exercise deleted."))
-//         .catch(err => res.status(400).json("Error:" + err));
-// });
-//
-// router.route("/update/:id").post((req, res) => {
-//     Exercise.findById(req.params.id)
-//         .then(exercise => {
-//             exercise.username = req.body.username;
-//             exercise.description = req.body.description;
-//             exercise.duration = Number(req.body.duration);
-//             exercise.date = Date.parse(req.body.date);
-//
-//             exercise
-//                 .save()
-//                 .then(() => res.json("Exercise updated!"))
-//                 .catch(err => res.status(400).json("Error: " + err));
-//         })
-//         .catch(err => res.status(400).json("Error:" + err));
-// });
+//route to get specific exercise
+router.route("/:id").get((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => res.json(exercise))
+        .catch(err => res.status(400).json("Error:" + err));
+});
+
+//route to delete one specific exercise
+router.route("/:id").delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json("Exercise deleted!"))
+        .catch(err => res.status(400).json("Error:" + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => {
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date);
+
+            exercise
+                .save()
+                .then(() => res.json("Welldone, Exercise updated!"))
+                .catch(err => res.status(400).json("Error: " + err));
+        })
+        .catch(err => res.status(400).json("Error:" + err));
+});
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,19 +1,22 @@
 const router = require("express").Router();
 let User = require("../models/user.model");
 
+//route for users
 router.route('/').get((req, res) => {
     User.find()// find returns a promise
         .then(users => res.json(users))
         .catch(err => res.status(400).json("Error:" + err));
 });
 
+
+//routes for adding new user
 router.route('/add').post((req, res) => {
     const username = req.body.username;
     const newUser = new User({username});
     console.log(newUser);
     newUser
         .save()
-        .then(() => res.json("user added"))
+        .then(() => res.json("Congrates! New user successfully added"))
         .catch(err => res.status(400).json("error:" + err));
 });
 
